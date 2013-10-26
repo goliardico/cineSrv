3b97f2f48b60b14ff14fbb4fac242e25fea5cf03
goliardico <goliardico@messiah.org>
2013-10-27 00:21:13 +0200

    Mongoose working, GET + PUT rubrica

--- app.js
+++ app.js
@@ -2,18 +2,42 @@ var express = require ('express');
 var app = express();
 
 var scrape = require('./scrape');
-
+var db = require('./db');
 
 // Produce il Json dei film in programmazione.
 // func: è generato dal client per proteggere dal CSS (vedi JSONP)
 // La chiamata da jQuery a questo servizio deve essere ($.getJSON):
 // http://<server>:3000/trovafilm?jsoncallback=?
 app.get ('/trovafilm', function(req, res) {
-   console.log('Ricevuta richiesta con func=' + req.query['jsoncallback']);
+   console.log('[trovafilm] func=' + req.query['jsoncallback']);
    scrape.trovaFilm( function(film) {
       res.send(film);
    }, req.query['jsoncallback']);
 });
+
+
+// Cerca all'interno del database l'esistenza di una precedente
+// registrazione per il telefono con uuid passato nella get
+// La richiesta dovrà essere:
+// http://<server>:3000/rubrica?uuid=?
+app.get ('/rubrica', function(req, res) {
+   db.rubrica( function(nome) {
+      res.send(nome);
+   }, 'get', req.query['uuid']);
+});
+
+// Inserisce un nuovo nome in rubrica
+// due parametri: nome e uuid
+// http://<server>:3000/rubrica?uuid=?&nome=?
+// restituisce:
+// ok : per inserimento corretto
+// nok: per errore
+app.put ('/rubrica', function(req, res) {
+   db.rubrica( function(risultato) {
+      res.send(risultato);
+   }, 'put', req.query['uuid'], req.query['nome']);
+});
+
 
 
 app.listen(3000);
--- db.js
+++ db.js
@@ -1,30 +1,59 @@
 var mongoose = require('mongoose');
 
-var usersColl = mongoose.Schema({
-   name: String,
+var usersSchema = mongoose.Schema({
+   nome: String,
    uuid: String
 });
 
-var favsColl = mongoose.Schema({
+var favsSchema = mongoose.Schema({
 	film: String,
 	cinema: String,
 	orario: String,
 	preferenze: Array
 });
 
+function handleError(error) {
+   console.log('Something wrong: ' + err);
+   callback('error');
+};
 
-function rubrica(callback, uuid) {
+// CRUD sulla rubrica dei nomi / uuid registrati
+function rubrica(callback, req, uuid, nome) {
 
    mongoose.connect('mongodb://localhost/cinesrv');
    var db = mongoose.connection;
 
-   db.on('error', console.error.bind(console, 'connection error:'));
-   db.once('open', function callback() {
-   	console.log('I\'m in!');
-   });
+   // associa lo schema alla collection all'interno del database
+   var usersModel = db.model('users', usersSchema);
+   // crea un documento in base allo schema (collection) del database
+   var users = new usersModel();
 
-   db.close();
-   callback('Well done!\n');
+   db.on('error', console.error.bind(console, 'connection error:'));
+   db.once('open', function () {
+
+      switch (req) {
+         case 'get':
+            console.log('[rubrica-get] search for: ' + uuid);
+            usersModel.findOne({ 'uuid': uuid }, 'nome', function (err, result) {
+               if (err) return handleError(err);
+               console.log('[rubrica-get] nome: ' + result.nome);
+               db.close();
+               callback(result.nome);
+            });
+         break;
+         case 'put':
+            console.log('[rubrica-put] insert: ' + nome);
+            users.nome = nome;
+            users.uuid = uuid;
+            users.save(function (err, users) {
+               if (err) return handleError(err);
+               console.log('[rubrica-put] salvato: ' + nome);
+               db.close();
+               callback('ok');
+            });
+         break;
+      };
+   });
 };
 
 exports.rubrica = rubrica;
